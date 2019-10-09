6240c17fe3b6440adfa33a2a1060ad48a00bd1d0
my99075 <491493327@qq.com>
2019-10-09 13:30:22 +0800

    no message

--- HelloWorldActivity3/app/src/main/java/com/example/helloworldactivity3/HelloWorldActivity1.java
+++ HelloWorldActivity3/app/src/main/java/com/example/helloworldactivity3/HelloWorldActivity1.java
@@ -3,11 +3,13 @@ package com.example.helloworldactivity3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 
 public class HelloWorldActivity1 extends AppCompatActivity {
+    public static final String TAG="HelloWorldActivity1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,4 +40,34 @@ public class HelloWorldActivity1 extends AppCompatActivity {
             }
         });
         }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+    }
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d(TAG,"onRestart");
+    }
     }
--- HelloWorldActivity3/app/src/main/java/com/example/helloworldactivity3/HelloWorldActivity3.java
+++ HelloWorldActivity3/app/src/main/java/com/example/helloworldactivity3/HelloWorldActivity3.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class HelloWorldActivity3 extends AppCompatActivity {
+    public static final String TAG="HelloWorldActivity3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +39,35 @@ public class HelloWorldActivity3 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+    }
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d(TAG,"onRestart");
     }
 }
